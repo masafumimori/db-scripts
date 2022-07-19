2331f231b340fb4f9ba3ad8cf02cfa823b9564d0
masafumimori <m.masafumimori@outlook.com>
2022-07-19 20:09:37 +0900

    add path for another java exe to use

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd ~/dynamodb
-java -Djava.library.path=./DynamoDBLocal_lib -jar ./DynamoDBLocal.jar -sharedDb
+# use x86-64 arch Java instead of for arm64
+# ref: https://stackoverflow.com/questions/66635424/dynamodb-local-setup-on-m1-apple-silicon-mac
+~/downloads/zulu11.56.19-ca-jre11.0.15-macosx_x64/bin/java -Djava.library.path=./DynamoDBLocal_lib -jar ./DynamoDBLocal.jar -sharedDb
